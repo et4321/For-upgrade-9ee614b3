4b33769bafc42859ea4cc11808fd963ac404545d
et4321 <54795466+et4321@users.noreply.github.com>
2019-10-09 11:37:09 +0200

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,13 @@
+<?php
+$klas = array(
+    "sjonnie de wiel",
+    "herman kaal",
+    "henk de steen",
+    "inge kerkhoven",
+    "gert kruiswijk",
+);
+foreach ($klas as &$value){
+    echo "$value";
+}
+
+?>
